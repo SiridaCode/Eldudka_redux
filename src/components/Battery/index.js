94771555d00c873b05ee247f4e9f4d7d003d9beb
Kirill <mox1toqq@gmail.com>
2022-02-06 01:42:50 +0300

    refactoring

--- src/components/Battery/index.js
+++ src/components/Battery/index.js
@@ -1,57 +1,15 @@
-import React from "react";
-import './styles.css'
+import React from 'react';
 
-const BatteryRender = ({ productValue }) => {
-    let productCount = '';
-    const parts = [];
+import './styles.css';
 
-    const productCountToProperies = {
-        'none': {
-            count: 0,
-            cssClass: ''
-        },
-        'extraLittle': {
-            count: 1,
-            cssClass: 'battery-red'
-        },
-        'little': {
-            count: 2,
-            cssClass: 'battery-yellow'
-        },
-        'normal': {
-            count: 3,
-            cssClass: 'battery-dark-green'
-        },
-        'many': {
-            count: 4,
-            cssClass: 'battery-green'
-        }
-    }
+const Battery = ({ items }) => {
+  return (
+    <div className="battery">
+      {items.map((item, index) => (
+        <div style={{ background: item }} key={index} className="battery__item"></div>
+      ))}
+    </div>
+  );
+};
 
-    if (productValue < 1) {
-        productCount = 'none';
-    } else if (productValue >= 1 && productValue <= 3) {
-        productCount = 'extraLittle';
-    } else if (productValue >= 4 && productValue <= 5) {
-        productCount = 'little';
-    } else if (productValue >= 6 && productValue <= 9) {
-        productCount = 'normal';
-    } else if (productValue >= 10) {
-        productCount = 'many';
-    }
-
-    const product = productCountToProperies[productCount];
-
-
-    for (let i = 0; i < product.count; i++) {
-        parts.push(product.cssClass);
-    }
-
-    return (
-        <React.Fragment>
-            {parts.map((cssClass, index) => <div key={index} className={cssClass} />)}
-        </React.Fragment>
-    )
-}
-
-export default BatteryRender;
+export default Battery;
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,56 +1,113 @@
 import * as React from 'react';
-import Battery from "../Battery";
-import './styles.css'
+import Battery from '../Battery';
+import './styles.css';
 
 const Card = ({ data, id }) => {
+  const nameCard = data.name.split('(');
+  const nameCardNoBracket = nameCard[0];
 
-    const nameCard = data.name.split('(');
-    const nameCardNoBracket = nameCard[0];
-
-    return (
-        <div className="product-item">
-            <div className="product-item-main-info">
-                <p id="product-name" className="product-item-name">{nameCardNoBracket}</p>
-                <div className="product-item-image">
-                    <img src='../image-card.png' className="product-item-image-text"></img>
-                </div>
-
-                <div className="product-item-availability" id="product-item-availability-galery">
-                    <div>Галерея: </div>
-                    <div className="battery-galery">
-                        <Battery productValue={data.availability && data.availability.galery} />
-                    </div>
-                    <div className="availability">{data.availability && data.availability.galery ? data.availability.galery : "Нет"}</div>
-                </div>
-
-                <div className="product-item-availability" id="product-item-availability-tuhach">
-                    <div>Тухачевского: </div>
-                    <div className="battery-tuhach">
-                        <Battery productValue={data.availability && data.availability.tuhach} />
-                    </div>
-                    <div className="availability">{data.availability && data.availability.tuhach ? data.availability.tuhach : "Нет"}</div>
-                </div>
-
-                <div className="product-item-availability" id="product-item-availability-kulakova">
-                    <div>Кулакова: </div>
-                    <div className="battery-kulakova">
-                        <Battery productValue={data.availability && data.availability.kulakova} />
-                    </div>
-                    <div className="availability">{data.availability && data.availability.kulakova ? data.availability.kulakova : "Нет"}</div>
-                </div>
-
-                <div className="product-item-availability" id="product-item-availability-shokolad">
-                    <div>Шоколад: </div>
-                    <div className="battery-shokolad">
-                        <Battery productValue={data.availability && data.availability.shokolad} />
-                    </div>
-                    <div className="availability">{data.availability && data.availability.shokolad ? data.availability.shokolad : "Нет"}</div>
-                </div>
-
-                <p className="product-item-price">{data.price + " ₽"}</p>
-            </div>
+  return (
+    <div className="product-item">
+      <div className="product-item-main-info">
+        <p id="product-name" className="product-item-name">
+          {nameCardNoBracket}
+        </p>
+        <div className="product-item-image">
+          <img src="../image-card.png" className="product-item-image-text"></img>
         </div>
-    )
-}
+
+        <div className="product-item-availability">
+          <div>Галерея</div>
+          <div className="battery-wrapper">
+            <Battery
+              items={
+                data.availability.galery < 1
+                  ? []
+                  : data.availability.galery >= 1 && data.availability.galery <= 3
+                  ? ['#BD1616']
+                  : data.availability.galery >= 4 && data.availability.galery <= 5
+                  ? ['#BD8E16', '#BD8E16']
+                  : data.availability.galery >= 6 && data.availability.galery <= 9
+                  ? ['#9CBD16', '#9CBD16', '#9CBD16']
+                  : ['#23BD16', '#23BD16', '#23BD16', '#23BD16']
+              }
+            />
+          </div>
+          <div className="availability">
+            {data.availability && data.availability.galery ? data.availability.galery : 'Нет'}
+          </div>
+        </div>
+
+        <div className="product-item-availability">
+          <div>Тухачевского</div>
+          <div className="battery-wrapper">
+            <Battery
+              items={
+                data.availability.tuhach < 1
+                  ? []
+                  : data.availability.tuhach >= 1 && data.availability.tuhach <= 3
+                  ? ['#BD1616']
+                  : data.availability.tuhach >= 4 && data.availability.tuhach <= 5
+                  ? ['#BD8E16', '#BD8E16']
+                  : data.availability.tuhach >= 6 && data.availability.tuhach <= 9
+                  ? ['#9CBD16', '#9CBD16', '#9CBD16']
+                  : ['#23BD16', '#23BD16', '#23BD16', '#23BD16']
+              }
+            />
+          </div>
+          <div className="availability">
+            {data.availability && data.availability.tuhach ? data.availability.tuhach : 'Нет'}
+          </div>
+        </div>
+
+        <div className="product-item-availability">
+          <div>Кулакова</div>
+          <div className="battery-wrapper">
+            <Battery
+              items={
+                data.availability.kulakova < 1
+                  ? []
+                  : data.availability.kulakova >= 1 && data.availability.kulakova <= 3
+                  ? ['#BD1616']
+                  : data.availability.kulakova >= 4 && data.availability.kulakova <= 5
+                  ? ['#BD8E16', '#BD8E16']
+                  : data.availability.kulakova >= 6 && data.availability.kulakova <= 9
+                  ? ['#9CBD16', '#9CBD16', '#9CBD16']
+                  : ['#23BD16', '#23BD16', '#23BD16', '#23BD16']
+              }
+            />
+          </div>
+          <div className="availability">
+            {data.availability && data.availability.kulakova ? data.availability.kulakova : 'Нет'}
+          </div>
+        </div>
+
+        <div className="product-item-availability">
+          <div>Шоколад</div>
+          <div className="battery-wrapper">
+            <Battery
+              items={
+                data.availability.shokolad < 1
+                  ? []
+                  : data.availability.shokolad >= 1 && data.availability.shokolad <= 3
+                  ? ['#BD1616']
+                  : data.availability.shokolad >= 4 && data.availability.shokolad <= 5
+                  ? ['#BD8E16', '#BD8E16']
+                  : data.availability.shokolad >= 6 && data.availability.shokolad <= 9
+                  ? ['#9CBD16', '#9CBD16', '#9CBD16']
+                  : ['#23BD16', '#23BD16', '#23BD16', '#23BD16']
+              }
+            />
+          </div>
+          <div className="availability">
+            {data.availability && data.availability.shokolad ? data.availability.shokolad : 'Нет'}
+          </div>
+        </div>
+
+        <p className="product-item-price">{data.price + ' ₽'}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
