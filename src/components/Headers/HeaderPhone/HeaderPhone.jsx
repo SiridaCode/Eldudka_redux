84fff81345911874d88dbbf6cd9133063a745e32
Sirida <nik.sviridov.1999@mail.ru>
2022-04-25 13:21:00 +0300

    responsive

--- src/components/Headers/HeaderPhone/HeaderPhone.jsx
+++ src/components/Headers/HeaderPhone/HeaderPhone.jsx
@@ -7,6 +7,9 @@ const HeaderPhone = () => {
     <header className="header-first">
       <Container>
         <div className="header-first-wrapper">
+          <div className="logo-mobile">
+            <img width={100} src="./logo.png" />
+          </div>
           <a className="call" href="tel:+79620100577">
             8 (962) 010 05-77
           </a>
