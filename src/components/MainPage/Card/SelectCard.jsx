9db5b3934d0607124f80bb5fff52f73e8d496dff
Sirida <nik.sviridov.1999@mail.ru>
2022-04-29 15:28:53 +0300

    fix_styles_select_card

--- src/components/MainPage/Card/SelectCard.jsx
+++ src/components/MainPage/Card/SelectCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import Container from '../../Container/Container';
 import './styles.css';
 
 const SelectCard = () => {
@@ -11,23 +12,25 @@ const SelectCard = () => {
   const [eng, rus] = value.name.split('(');
 
   return (
-    <>
-      <div className="product-item-select">
-        <div>{eng}</div>
-        <div>{'(' + rus}</div>
-        <div>{value.price + ' ₽'}</div>
-        <div>{'Кулакова: ' + value.availability.kulakova ?? 'Нет'}</div>
-        <div>{'Тухачевского: ' + value.availability.tuhach ?? 'Нет'}</div>
-        <div>{'Шоколад: ' + value.availability.shokolad ?? 'Нет'}</div>
-        <img
-          src={value.pic ? value.pic : '../image-card.png'}
-          className="product-item-image-text"
-        />
-        <a href="https://vk.com/vapestav" className="to-order">
-          Заказать
-        </a>
-      </div>
-    </>
+    <div className="cards-wrapper">
+      <Container>
+        <div className="product-item-select">
+          <div>{eng}</div>
+          <div>{'(' + rus}</div>
+          <div>{value.price + ' ₽'}</div>
+          <div>{'Кулакова: ' + value.availability.kulakova ?? 'Нет'}</div>
+          <div>{'Тухачевского: ' + value.availability.tuhach ?? 'Нет'}</div>
+          <div>{'Шоколад: ' + value.availability.shokolad ?? 'Нет'}</div>
+          <img
+            src={value.pic ? value.pic : '../image-card.png'}
+            className="product-item-image-text"
+          />
+          <a href="https://vk.com/vapestav" className="to-order">
+            Заказать
+          </a>
+        </div>
+      </Container>
+    </div>
   );
 };
 
