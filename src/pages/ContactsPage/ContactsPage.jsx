b09649d846dc6869ae35e49207415b05b16570ca
Sirida <nik.sviridov.1999@mail.ru>
2022-05-19 12:36:41 +0300

    contacts_style

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -4,9 +4,9 @@ import Container from '../../components/Container/Container';
 import { Link } from 'react-router-dom';
 import './styles.css';
 import { src } from '../../utils/utils';
-import SimpleAccordion from './Accordion';
 import { useDispatch } from 'react-redux';
 import { setBreadcrumbs } from '../../redux/data/dataActions';
+import Paper from '@mui/material/Paper';
 
 const ContactPage = () => {
   const dispatch = useDispatch();
@@ -14,42 +14,43 @@ const ContactPage = () => {
     <div className="cards-wrapper">
       <Container>
         <div className="contacts-container">
-          <div className="header-contacts">Режим работы:</div>
-          <div>
+          <div className="header-contacts">Режим Работы офлайн магазинов в Ставрополе:</div>
+          <Paper>
             <span>ТЦ Галерея 2 этаж</span>
             <br /> ул.Маршала Жукова 8<br /> 10:30 - 20:30
-          </div>
-          <div>
+          </Paper>
+          <Paper>
             <span>ул.Тухачевского 19/3</span>
             <br /> 10:00 - 22:00
-          </div>
-          <div>
+          </Paper>
+          <Paper>
             <span>Гипермаркет Закрома цоколь</span>
             <br /> Серова 468/5
             <br /> 10:00 - 20:00
-          </div>
-          <div>
+          </Paper>
+          <Paper>
             <span>ЖК ШОКОЛАД</span>
             <br /> Крупской 29
             <br /> 10:30 - 20:00
-          </div>
-          <div>
+          </Paper>
+          <Paper>
             <span>Макарова 22 1 этаж</span>
             <br /> 11:00-20:00
-          </div>
-          {/* <SimpleAccordion /> */}
-          <div className="icon-container">
+          </Paper>
+          <Paper className="icon-container">
             {src.map(({ href, src }) => (
               <a href={href}>
                 <img className="icon" src={src} alt="icon" />
               </a>
             ))}
-          </div>
-          <Link onClick={() => dispatch(setBreadcrumbs([{ name: 'Главная', href: '/' }]))} to="/">
-            <Button color="error" variant="outlined">
-              Вернуться
-            </Button>
-          </Link>
+          </Paper>
+          <Paper>
+            <Link onClick={() => dispatch(setBreadcrumbs([{ name: 'Главная', href: '/' }]))} to="/">
+              <Button color="error" variant="outlined">
+                Вернуться
+              </Button>
+            </Link>
+          </Paper>
         </div>
       </Container>
     </div>
