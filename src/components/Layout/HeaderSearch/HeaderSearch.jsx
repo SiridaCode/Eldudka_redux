5b38ee6f6601ecda8d0d8da07a6584af32e68fb3
Sirida <nik.sviridov.1999@mail.ru>
2022-05-26 13:47:28 +0300

    add select-search-component

--- src/components/Layout/HeaderSearch/HeaderSearch.jsx
+++ src/components/Layout/HeaderSearch/HeaderSearch.jsx
@@ -1,70 +1,9 @@
 import * as React from 'react';
 import './styles.css';
-import cn from 'classnames';
-import { useDispatch, useSelector } from 'react-redux';
-import { filteredProductsBySearch } from '../../../utils/filter';
-import {
-  setFilterData,
-  setCurrentPage,
-  setSearchData,
-  setBreadcrumbs,
-} from '../../../redux/data/dataActions';
 import Container from '../../Container/Container';
-import { useHistory } from 'react-router-dom';
+import SelectSearch from './SelectSearch';
 
 const HeaderSearch = () => {
-  const [openSearch, setOpenSearch] = React.useState(false);
-  const [searchText, setSearchText] = React.useState('');
-  const { fullData, searchData } = useSelector(({ data }) => data);
-  const dispatch = useDispatch();
-  const history = useHistory();
-
-  const onChangeSearch = ({ target }) => {
-    dispatch(setSearchData(filteredProductsBySearch(fullData, target.value)));
-    setSearchText(target.value);
-    history.push('/');
-    dispatch(setBreadcrumbs([{ name: 'Главная', href: '/' }]));
-  };
-
-  React.useEffect(() => {
-    const isOpen = searchText ? true : false;
-    setOpenSearch(isOpen);
-  }, [searchText]);
-
-  const onClickElementSearch = (eng, id) => {
-    setSearchText('');
-    setOpenSearch(false);
-    history.push('search' + id);
-    dispatch(
-      setBreadcrumbs([
-        { name: 'Главная', href: '/' },
-        { name: 'Карточка товара', href: `/search${id}` },
-      ])
-    );
-
-    dispatch(setCurrentPage(0));
-  };
-
-  const onClickDeleteTarget = () => {
-    setSearchText('');
-    setOpenSearch(false);
-    dispatch(setFilterData(fullData));
-    dispatch(setSearchData(fullData));
-  };
-
-  const onClickSelectAll = () => {
-    dispatch(setFilterData(searchData));
-    setSearchText('');
-    dispatch(setCurrentPage(0));
-    setOpenSearch(false);
-    history.push('/');
-    dispatch(setBreadcrumbs([{ name: 'Главная', href: '/' }]));
-  };
-
-  const onClickSearch = () => {
-    setOpenSearch(!openSearch);
-  };
-
   return (
     <header className="header-second">
       <Container>
@@ -72,47 +11,7 @@ const HeaderSearch = () => {
           <div className="logo">
             <img src="../logo.png" className="logo" alt="logo"></img>
           </div>
-          <div className="input-wrapper">
-            <input
-              onClick={onClickSearch}
-              onChange={onChangeSearch}
-              value={searchText}
-              type="text"
-              className="js-data-example-ajax"
-              name="state"
-              placeholder="Поиск..."
-            />
-            <div className={cn({ search: openSearch === true })}>
-              {openSearch &&
-                searchData &&
-                searchData.map((value, id) => {
-                  const [eng, rus] = value.name.split('(');
-                  return (
-                    <div
-                      id={id}
-                      key={id}
-                      onClick={() => onClickElementSearch(eng, id)}
-                      className="element-search"
-                    >
-                      {eng}
-                    </div>
-                  );
-                })}
-            </div>
-            {openSearch === true && (
-              <div onClick={onClickSelectAll} className="delete-target">
-                Выбрать все
-              </div>
-            )}
-            {openSearch === true && (
-              <img
-                onClick={onClickDeleteTarget}
-                className="vector"
-                src="../Vector.png"
-                alt="no image"
-              />
-            )}
-          </div>
+          <SelectSearch />
         </div>
       </Container>
     </header>
